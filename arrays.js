a46c5c45091fde0206803967f8fcfa326c87c5f3
Cedric Niguidula <cedcn-9mail@yahoo.com>
2017-09-30 05:48:37 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -27,6 +27,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
   return array;
 }
 
-function RemoveElementFromBeginningOfArray(array){
+function removeElementFromBeginningOfArray(array){
   return array.slice[1];
 }
